efacfcd0cae868772b75f9f80b6516c4150a7964
3shao <23442735+wzzjhc@users.noreply.github.com>
2026-06-09 19:53:29 +0800

    Create good.sql

---
+++ good.sql
@@ -0,0 +1,8 @@
+{
+	"0000001": {
+		"name": "NPPJSONViewer.dll",
+		"download": "https://github.com/NPP-JSONViewer/JSON-Viewer/releases",
+		"Description": "Copy the file NPPJSONViewer.dll to the plugins-NPPJSONViewer folder in the Notepad++ installation directory..Restart Notepad++ and ensure the plugin appears under the Plugins menu."
+	},
+	"type": "ok"
+}
